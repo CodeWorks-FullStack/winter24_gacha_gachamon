68f1af4272ea5f7ee741f62eff08e831d43f68ae
Jeremy Fowler <jembyfowler@gmail.com>
2024-11-05 11:20:41 -0700

    created gachamon class

---
+++ app/models/Gachamon.js
@@ -0,0 +1,10 @@
+
+export class Gachamon {
+  constructor(data) {
+    this.name = data.name
+    this.rarity = data.rarity
+    this.emoji = data.emoji
+  }
+}
+
+// new Gachamon({ name: 'herbert', rarity: 'common', emoji: '🦍' })
