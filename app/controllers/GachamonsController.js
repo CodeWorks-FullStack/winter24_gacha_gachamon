700b0a1d4578c1f9d8158271f1d717accb8a195d
Jeremy Fowler <jembyfowler@gmail.com>
2024-11-05 11:52:00 -0700

    drawing emojis

--- app/controllers/GachamonsController.js
+++ app/controllers/GachamonsController.js
@@ -1,7 +1,18 @@
+import { AppState } from "../AppState.js";
 
 export class GachamonsController {
   constructor() {
     console.log('Gachamons controller is loaded!');
 
+  }
+
+  drawGachamons() {
+    const gachamons = AppState.gachamons
+    console.log('drawing!', gachamons);
+    let gachamonEmojis = ''
+    gachamons.forEach(gachamon => gachamonEmojis += gachamon.emoji)
+    console.log('emojis!', gachamonEmojis);
+    const gachamonsCatalogElem = document.getElementById('gachamon-catalog')
+    gachamonsCatalogElem.innerText = gachamonEmojis
   }
 }
