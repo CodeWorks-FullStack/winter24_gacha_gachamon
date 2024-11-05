f656a6bfd043a79af0e96a353817a5710aab039a
Jeremy Fowler <jembyfowler@gmail.com>
2024-11-05 11:27:52 -0700

    added gachamons to appstate

--- app/AppState.js
+++ app/AppState.js
@@ -1,12 +1,31 @@
+import { Gachamon } from './models/Gachamon.js'
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
 class ObservableAppState extends EventEmitter {
-
-  /**@type {import('./models/Example.js').Example[]} */
-  examples = []
-
   coins = 0
+  gachamons = [
+    new Gachamon({
+      name: 'Fred',
+      rarity: 'common',
+      emoji: '🐃'
+    }),
+    new Gachamon({
+      name: 'Herbert',
+      rarity: 'common',
+      emoji: '🦃'
+    }),
+    new Gachamon({
+      name: 'Walter',
+      rarity: 'uncommon',
+      emoji: '🐖'
+    }),
+    new Gachamon({
+      name: 'Mick & Jeremy',
+      rarity: 'rare',
+      emoji: '🏇'
+    }),
+  ]
 }
 
 export const AppState = createObservableProxy(new ObservableAppState())
